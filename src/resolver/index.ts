ff21204ce9d22984c84ad6a7e64a87b3bf02c1e5
hienlh <hienlh1298@gmail.com>
2019-12-17 21:37:49 +0700

    Complete daily report resolver

--- src/resolver/index.ts
+++ src/resolver/index.ts
@@ -1,10 +1,17 @@
 import { CustomerResolver } from './customer.resolver';
 import { DailyDishResolver } from './dailyDish.resolver';
+import { DailyReportResolver } from './dailyReport.resolver';
 import { ImportBillResolver } from './importBill.resolver';
 import { StoreResolver } from './store.resolver';
 import { UserResolver } from './user.resolver';
 import { WarehouseResolver } from './warehouse.resolver';
 
 export const resolvers = [
-    UserResolver, CustomerResolver, StoreResolver, DailyDishResolver, WarehouseResolver, ImportBillResolver
+    UserResolver,
+    CustomerResolver,
+    StoreResolver,
+    DailyDishResolver,
+    WarehouseResolver,
+    ImportBillResolver,
+    DailyReportResolver
 ];
